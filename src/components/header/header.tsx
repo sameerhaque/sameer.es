c1c7d7bd37034ca07da1745dfdb511090beed81f
Sameer Haque <sameer.haque@sonnet.ca>
2020-01-24 13:28:45 -0500

    updated header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,75 +3,77 @@ import 'bulmil';
 
 @Component({
   tag: 'sameer-header',
+  styleUrl: 'header.scss',
   shadow: true
 })
 export class SameerHeader {
   render() {
     return (
-        <bm-navbar>
-        <div class="navbar-brand">
-          <h1>Sameer Haque</h1>
-{/* 
-          <a
-            role="button"
-            class="navbar-burger burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a> 
-        </div>
-        
-        <div id="navbarBasicExample" class="navbar-menu">
-          <div class="navbar-start">
-            <a class="navbar-item">
-              Home
-            </a>
-
-            <a class="navbar-item">
-              Documentation
-            </a>
-
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link">
-                More
+        <bm-navbar color={"is-light"} >
+          <bm-container is-fluid="false" class={"container"} breakpoint={"is-widescreen"}>
+            <div class="navbar-brand">
+              <h1>Sameer Haque</h1>
+              <a
+                role="button"
+                class="navbar-burger burger"
+                aria-label="menu"
+                aria-expanded="false"
+                data-target="navbarBasicExample"
+              >
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
               </a>
+            </div>
 
-              <div class="navbar-dropdown">
+            <div id="navbarBasicExample" class="navbar-menu">
+              <div class="navbar-start">
                 <a class="navbar-item">
-                  About
+                  Home
                 </a>
+
                 <a class="navbar-item">
-                  Jobs
+                  Documentation
                 </a>
-                <a class="navbar-item">
-                  Contact
-                </a>
-                <hr class="navbar-divider" />
-                <a class="navbar-item">
-                  Report an issue
-                </a>
+
+                <div class="navbar-item has-dropdown is-hoverable">
+                  <a class="navbar-link">
+                    More
+                  </a>
+
+                  <div class="navbar-dropdown">
+                    <a class="navbar-item">
+                      About
+                    </a>
+                    <a class="navbar-item">
+                      Jobs
+                    </a>
+                    <a class="navbar-item">
+                      Contact
+                    </a>
+                    <hr class="navbar-divider" />
+                    <a class="navbar-item">
+                      Report an issue
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="navbar-end">
+                <div class="navbar-item">
+                  <bm-buttons>
+                    <bm-button color="is-primary">
+                      <strong>Sign up</strong>
+                    </bm-button>
+                    <bm-button color="is-light">
+                      Log in
+                    </bm-button>
+                  </bm-buttons>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <bm-buttons>
-                <bm-button color="is-primary">
-                  <strong>Sign up</strong>
-                </bm-button>
-                <bm-button color="is-light">
-                  Log in
-                </bm-button>
-              </bm-buttons>
-            </div>
-          </div>*/}
-        </div> 
-      </bm-navbar> 
+          </bm-container>
+        </bm-navbar>
     );
   }
 }
--- stencil.config.ts
+++ stencil.config.ts
@@ -1,16 +1,18 @@
 import { Config } from '@stencil/core';
-
-// https://stenciljs.com/docs/config
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
   globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
+  plugins: [
+    sass()
+  ],
   outputTargets: [
     {
       type: 'www',
       // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: 'https://myapp.local/'
+      baseUrl: 'https://sameer.es/'
     }
   ]
 };
